5e221923e235efde50cb26161fca15455b5b6f31
JoaoVitor <contato.java@hotmail.com>
2026-06-23 23:10:02 -0300

    fix: update API URL for production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiUrl: (typeof window !== 'undefined' && window.location.hostname === 'localhost')
         ? 'http://localhost:8000'
-        : 'https://apidash.grupomillion.com'
+        : 'https://videodashboardapigiovani.ignorelist.com'
 }; 
